ddf640f5f2097182ceaab8c2e42db7883fce4316
Kyle Kalantzis <kmk1052@wildcats.unh.edu>
2022-01-08 05:31:01 -0500

    add base page for UNH student database

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// Making connection to XAMP
+$servername = "127.0.0.1";
+$username = "root";
+$password = "Kyle1996";
+$database = "PNSDatabase";
+
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected to the PNS Database";  
+?>
+<!DOCTYPE HTML>
+<HEAD>
+<!-- Insert CSS ref here -->
+<meta charset = "UTF-8">
+<meta http-equiv ="X-UA-Compatible" content="IE=edge">
+<meta name = "viewport" content = "width=device-width, initial-scale = 1.0">
+</head>
